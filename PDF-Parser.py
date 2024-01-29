d9a09163fff7d312e2dd59bdb80d1e2ab2c9f58f
Braden H <106448224+brhall13@users.noreply.github.com>
2024-01-29 17:57:42 -0500

    Fixed Parser
    Used deprecated class from PyPDF2 library, adjusted to PdfReader

--- PDF-Parser.py
+++ PDF-Parser.py
@@ -4,20 +4,22 @@ def pdf_to_text(file_path):
     # Open the PDF file in binary mode
     with open(file_path, 'rb') as file:
         # Create a PDF file reader object
-        pdf_reader = PyPDF2.PdfFileReader(file)
+        pdf_reader = PyPDF2.PdfReader(file)
         
         # Initialize an empty string to store the text
         text = ''
         
         # Loop through each page in the PDF
-        for page_num in range(pdf_reader.numPages):
+        for page in pdf_reader.pages:
             # Extract the text from the page
-            page = pdf_reader.getPage(page_num)
-            text += page.extractText()
+            text += page.extract_text()
         
     return text
 
 # Change variable to a command argument later
-file_path = 'path_to_your_pdf_file.pdf'
+file_path = 'functionalsample.pdf'
 text = pdf_to_text(file_path)
 print(text)
+
+if __name__ == '__main__':
+    print(pdf_to_text(file_path))
