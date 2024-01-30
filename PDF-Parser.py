dd7ae78131ee3f75b18fa7b25f70144cc7825939
Braden H <106448224+brhall13@users.noreply.github.com>
2024-01-30 12:08:32 -0500

    GPT Chat Completions

--- PDF-Parser.py
+++ PDF-Parser.py
@@ -1,25 +1,39 @@
 import PyPDF2
+from openai import OpenAI
+
 
 def pdf_to_text(file_path):
     # Open the PDF file in binary mode
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         # Create a PDF file reader object
         pdf_reader = PyPDF2.PdfReader(file)
-        
+
         # Initialize an empty string to store the text
-        text = ''
-        
+        text = ""
+
         # Loop through each page in the PDF
         for page in pdf_reader.pages:
             # Extract the text from the page
             text += page.extract_text()
-        
+
     return text
 
-# Change variable to a command argument later
-file_path = 'functionalsample.pdf'
-text = pdf_to_text(file_path)
-print(text)
 
-if __name__ == '__main__':
-    print(pdf_to_text(file_path))
+# Calls OpenAI API to summarize the text
+def send_chat(resume_text):
+    openai = OpenAI()
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", "content": "You are an AI assistant helping summarize resume, designed to output JSON."},
+            {"role": "user", "content": resume_text}]
+    )
+    # print(response)
+    return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    file_path = "functionalsample.pdf"
+    response = send_chat(pdf_to_text(file_path))
+    print(response)
